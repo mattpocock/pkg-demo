a493d5f5ecd9d0847347073f5e550247dac11a20
DaMiLaReE-mIcHaElL <damilareemichaell@gmail.com>
2023-01-21 22:09:08 +0100

    🆕 --changed api

---
+++ .changeset/silly-eagles-notice.md
@@ -0,0 +1,10 @@
+---
+"my-package": major
+---
+
+converted add function to Add class
+
+
+# changes
+    - breaks
+        add( 5, 6 ) won't work again use instead new Add( 5, 6 ) 
--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-export const add = (a: number, b: number) => {
-  return a + b
+export class Add {
+  constructor ( private a: number, private b: number ) {
+    this.a = a;
+    this.b = b;
+    this.run();
+  }
+  run (): number {
+    return this.a + this.b;
+  }
 }
