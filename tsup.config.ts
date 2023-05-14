38910ab2735049bdaf765e176b4b4823b0e0d8d1
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-05-14 15:22:14 +0300

    Fixed tsup file styling

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,14 @@
-import type { Options } from 'tsup';
+import type { Options } from "tsup";
 
 export const tsup: Options = {
-    splitting: true,
-    clean: true, // clean up the dist folder
-    dts: true, // generate dts files
-    format: ['cjs', 'esm'], // generate cjs and esm files
-    minify: true,
-    bundle: true,
-    skipNodeModulesBundle: true,
-    target: 'es2016',
-    outDir: 'dist',
-    entry: ['src/**/*.ts'], //include all files under src
+  splitting: true,
+  clean: true, // clean up the dist folder
+  dts: true, // generate dts files
+  format: ["cjs", "esm"], // generate cjs and esm files
+  minify: true,
+  bundle: true,
+  skipNodeModulesBundle: true,
+  target: "es2016",
+  outDir: "dist",
+  entry: ["src/**/*.ts"], //include all files under src
 };
