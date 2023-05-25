36f517fb0dae8f148947709cc1faec88d624a1cd
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-05-25 09:03:28 +0300

    Fixed peers deps import

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Dispatch, SetStateAction, useCallback, useMemo } from "react";
 // This doesn't work in ESM, because use-sync-external-store only exposes CJS.
 // See: https://github.com/pmndrs/valtio/issues/452
 // The following is a workaround until ESM is supported.
-import useSyncExternalStoreExports from "use-sync-external-store/shim/index";
+import useSyncExternalStoreExports from "use-sync-external-store/shim/index.js";
 import { getStoreMock } from "./server-store-mock";
 const { useSyncExternalStore } = useSyncExternalStoreExports;
 
